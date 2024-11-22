14635523ccd909f3728e3fe7b152c1acab7e6556
FreelancerMamun <mamunkhan16345@gmail.com>
2024-11-22 09:53:05 +0600

    update

---
+++ src/components/productCard/productCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from 'next/image';
+const ProductCard = () => {
+  return (
+    <div className="bg-white p-2">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/util/productData.js
@@ -0,0 +1,28 @@
+const productData = [
+  {
+    id: 1,
+    name: '',
+    price: '',
+    image: '',
+  },
+  {
+    id: 2,
+    name: '',
+    price: '',
+    image: '',
+  },
+  {
+    id: 3,
+    name: '',
+    price: '',
+    image: '',
+  },
+  {
+    id: 4,
+    name: '',
+    price: '',
+    image: '',
+  },
+];
+
+export default productData;
