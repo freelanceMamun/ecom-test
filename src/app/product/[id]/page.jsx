66b9f40c34392c33a1b846eded733d92b04aea8c
FreelancerMamun <mamunkhan16345@gmail.com>
2024-11-23 23:48:56 +0600

    update

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+'use client';
+import React from 'react';
+
+import staricon from '@/asset/star1.svg';
+import staricon1 from '@/asset/star-w.svg';
 
 const DetailProduct = () => {
-  return <div>DetailProduct</div>;
+  return (
+    <div className="mx-auto max-w-screen-xl p-4">
+      <div className="">
+        {/* nav Breadcum */}
+        <div className=" py-4">
+          <a href="#">Feature Product / </a>
+          <a href="#" className="text-[#7E53D4] font-medium">
+            New Arrival
+          </a>
+        </div>
+        {/* nav Breadcum */}
+        <div className="grid grid-cols-2">
+          <div className=""></div>
+          <div className="">
+            <div className="pb-3">
+              <button className="bg-[#2F1C59] rounded-lg px-14 py-3 text-white">
+                {' '}
+                New Arrival
+              </button>
+            </div>
+            <div className="productName">
+              <h4 className="text-[33px] font-semibold">White Hoodie</h4>
+              <div className="flex gap-2 py-5 items-center">
+                <div className="ratting">
+                  <div className="flex gap-1">
+                    <img src={staricon.src} alt="star" />
+                    <img src={staricon.src} alt="star" />
+                    <img src={staricon.src} alt="star" />
+                    <img src={staricon.src} alt="star" />
+                    <img src={staricon1.src} alt="star" />
+                  </div>
+                </div>
+                <div className="flex  gap-2">
+                  <p>(4.0)</p>
+                  <p className="text-[#7E53D4]">121 reviews</p>
+                </div>
+              </div>
+              <div className="price">
+                <h3 className="text-[28px] font-bold">BDT 2500</h3>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 py-8">
+              <div>
+                <h5 className="text-[19px] font-medium pb-3">Available Size</h5>
+                <div className="flex gap-2">
+                  <button className=" py-2 px-4 border rounded-md border-[#7E53D4]">
+                    S
+                  </button>
+                  <button className="py-2 px-4 rounded-md">M</button>
+                  <button className=" py-2 px-4 rounded-md">L</button>
+                  <button className=" py-2 px-4 rounded-md">XL</button>
+                </div>
+              </div>
+              <div>
+                <h5 className="text-[19px] font-medium pb-3">
+                  Available Color
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailProduct;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,102 +9,103 @@ import shopinglogo from '../../asset/shopping.svg';
 import menuIcon from '../../asset/menu-02.svg';
 const Header = () => {
   return (
-    <header className="bg-[#F5F3FF] container mx-auto max-w-screen-xl">
-      <div className="container mx-auto flex justify-between items-center p-4">
-        <a href="#" className="lg:hidden text-[25px] mr-3">
-          <img src={menuIcon.src} alt="menu" />
-        </a>
-        <a href="#" className="text-2xl font-bold">
-          <img
-            className="w-[140px] md:w-full"
-            src={Logo.src}
-            alt="brand-logo"
-          />
-        </a>
+    <header className="bg-[#F6F5FD]">
+      <div className="container mx-auto max-w-screen-xl">
+        <div className="container mx-auto flex justify-between items-center p-4">
+          <a href="#" className="lg:hidden text-[25px] mr-3">
+            <img src={menuIcon.src} alt="menu" />
+          </a>
+          <a href="#" className="text-2xl font-bold">
+            <img
+              className="w-[140px] md:w-full"
+              src={Logo.src}
+              alt="brand-logo"
+            />
+          </a>
 
-        <div className="lg:flex items-center flex-1 justify-between hidden ">
-          <nav className="hidden flex-1 justify-center md:flex space-x-6">
-            <a
-              href="#home"
-              className="text-[#581FC1] font-semibold text-[16px]"
-            >
+          <div className="lg:flex items-center flex-1 justify-between hidden ">
+            <nav className="hidden flex-1 justify-center md:flex space-x-6">
+              <a
+                href="#home"
+                className="text-[#581FC1] font-semibold text-[16px]"
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
+              >
+                Shop
+              </a>
+              <a
+                href="#services"
+                className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
+              >
+                Deals
+              </a>
+              <a
+                href="#contact"
+                className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
+              >
+                What's New
+              </a>
+            </nav>
+
+            <div className="mx-4">
+              <input
+                type="text"
+                placeholder="Search products..."
+                className="px-4 py-2 bg-white rounded-full text-gray-800  focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+          </div>
+
+          <div className="flex-1 md:flex-none justify-end flex">
+            <div className="flex gap-4 md:gap-5">
+              <div className="lg:hidden ">
+                <a href="#" className="text-[24px]">
+                  <FiSearch />
+                </a>
+              </div>
+              <div className="">
+                <a href="#" className="text-[24px] relative">
+                  <span className="w-5 h-5 text-white absolute right-[-8px] top-[-5px] bg-black rounded-full flex justify-center items-center text-[11px]">
+                    10
+                  </span>
+                  <img src={shopinglogo.src} alt="shoping" />
+                </a>
+              </div>
+              <div className="">
+                <a href="#">
+                  <img src={userLogo.src} alt="user"></img>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <button
+            id="mobile-menu-btn"
+            className="block md:hidden text-2xl focus:outline-none"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <div id="mobile-menu" className="hidden md:hidden bg-gray-700">
+          <nav className="flex flex-col space-y-4 p-4">
+            <a href="#home" className="hover:text-blue-400">
               Home
             </a>
-            <a
-              href="#about"
-              className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
-            >
-              Shop
+            <a href="#about" className="hover:text-blue-400">
+              About
             </a>
-            <a
-              href="#services"
-              className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
-            >
-              Deals
+            <a href="#services" className="hover:text-blue-400">
+              Services
             </a>
-            <a
-              href="#contact"
-              className="hover:text-blue-400 text-[#646464] font-semibold text-[16px]"
-            >
-              What's New
+            <a href="#contact" className="hover:text-blue-400">
+              Contact
             </a>
           </nav>
-
-          <div className="mx-4">
-            <input
-              type="text"
-              placeholder="Search products..."
-              className="px-4 py-2 bg-white rounded-full text-gray-800  focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
         </div>
-
-        <div className="flex-1 md:flex-none justify-end flex">
-          <div className="flex gap-4 md:gap-5">
-            <div className="lg:hidden ">
-              <a href="#" className="text-[24px]">
-                <FiSearch />
-              </a>
-            </div>
-            <div className="">
-              <a href="#" className="text-[24px] relative">
-                <span className="w-5 h-5 text-white absolute right-[-8px] top-[-5px] bg-black rounded-full flex justify-center items-center text-[11px]">
-                  10
-                </span>
-                <img src={shopinglogo.src} alt="shoping" />
-              </a>
-            </div>
-            <div className="">
-              <a href="#">
-                <img src={userLogo.src} alt="user"></img>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <button
-          id="mobile-menu-btn"
-          className="block md:hidden text-2xl focus:outline-none"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-      </div>
-
-      <div id="mobile-menu" className="hidden md:hidden bg-gray-700">
-        <nav className="flex flex-col space-y-4 p-4">
-          <a href="#home" className="hover:text-blue-400">
-            Home
-          </a>
-          <a href="#about" className="hover:text-blue-400">
-            About
-          </a>
-          <a href="#services" className="hover:text-blue-400">
-            Services
-          </a>
-          <a href="#contact" className="hover:text-blue-400">
-            Contact
-          </a>
-        </nav>
       </div>
     </header>
   );
