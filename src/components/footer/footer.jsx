70343ec88bba88e1308550135ea4ed17285e04b8
FreelancerMamun <mamunkhan16345@gmail.com>
2024-11-23 20:05:07 +0600

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import Logo from '../../asset/logowhite.png';
+import Logo from '../../asset/logo (3).png';
 const Footer = () => {
   return (
     <footer className="">
-      <div className="bg-black">
-        <div className="container mx-auto flex justify-between items-center p-4">
-          <div className="grid">
-            <div className="">
+      <div className="bg-black py-10">
+        <div className="container mx-auto max-w-screen-xl flex justify-between items-center p-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="max-w-md">
               <div>
                 <a href="/">
-                  <img src={Logo.src} alt="Brand Logo" />
+                  <img
+                    className="w-full md:w-[195px]"
+                    src={Logo.src}
+                    alt="Brand Logo"
+                  />
                 </a>
               </div>
+              <div className="py-6">
+                <p className="text-white text-[16px]">
+                  Saepe quo suscipit vitae quia. Repudiandae nobis quis.
+                  Laboriosam unde quae qui quasi mollitia tenetur. Dicta
+                  explicabo est consectetur maxime quos fugit velit assumenda
+                  est.
+                </p>
+              </div>
             </div>
-            <div></div>
+            <div>
+              <div>
+                <h4 className="text-white text-[23px]">
+                  Sign Up For Our Newsletter!
+                </h4>
+                <p className="text-white text-[16px]">
+                  Get notified about updates and be the first to get early
+                  access to new Podcast episodes.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
